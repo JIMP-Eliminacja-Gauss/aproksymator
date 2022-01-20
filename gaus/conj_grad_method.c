57cddd3fa8ac04118d0ca5809a88806cde6c5fb7
Wiktor Nasierowski <wiktor.nasierowski@gmail.com>
2022-01-20 23:20:45 +0100

    TODO funckja dodajaca macierze

--- gaus/conj_grad_method.c
+++ gaus/conj_grad_method.c
@@ -2,12 +2,14 @@
 
 int 
 check_mat_sym(matrix_t *mat) {
+    int i, j;
+
     if (mat->cn != mat->rn) {
         return -1;
     }
 
-    for (int i = 0; i < mat->rn; i++) {
-        for (int j = 0; j < mat->cn; j++) {
+    for (i = 0; i < mat->rn; i++) {
+        for (j = 0; j < mat->cn; j++) {
             if (mat->e[i * mat->cn + j] != mat->e[j * mat->cn + j]) {
                 return 0;
                 // meaning matrix is not symmetrical
@@ -19,8 +21,25 @@ check_mat_sym(matrix_t *mat) {
     // matrix is indeed symmetrical
 }
 
+matrix_t *
+matrix_substract(matrix_t *a, matrix_t *b) {
+    return NULL;
+
+}
+
 
 matrix_t * 
 conj_grad_solver(matrix_t *mat) {
-    
+    int k = 0;
+    int end = mat->rn;
+    matrix_t *x_mat = make_matrix(mat->cn, 1);
+    matrix_t *r_mat = make_matrix(mat->cn, 1);
+    matrix_t *p_mat = copy_matrix(r_mat);
+
+
+    for (; k < end; k++) {
+        ;
+    }
+
+    return NULL;
 }
